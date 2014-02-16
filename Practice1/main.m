b585cf5d671b1de1168cc626ca2a53d71434bf4f
strmj02 <strmj02@moravian.eu>
2014-02-16 13:41:35 -0500

    Initial Commit

---
+++ Practice1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Practice1
+//
+//  Created by Rachel Johnson on 2/16/14.
+//  Copyright (c) 2014 Rachel Johnson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
